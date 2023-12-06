e3d8cf1ccf3e43f80d3107441932b839415122c5
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2023-12-06 21:36:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // your code here
+ document.getElementById('myForm').addEventListener('submit', function(event) {
+      event.preventDefault(); // Prevent the default form submission
+      
+      // Get values from the form
+      var nameValue = document.getElementById('name').value;
+      var yearValue = document.getElementById('year').value;
+
+      // Construct the new URL with the query string
+      var newUrl = 'https://localhost:8080/?name=' + encodeURIComponent(nameValue) + '&year=' + encodeURIComponent(yearValue);
+
+      // Update the h3 element with the new URL
+      document.getElementById('url').textContent = newUrl;
+    });
